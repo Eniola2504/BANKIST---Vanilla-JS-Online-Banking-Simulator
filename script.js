4d99c41b03410accc1cd99eac5829bcb2c0a30c8
Enyholla <sannifawaz2504@gmail.com>
2026-01-19 01:13:14 +0100

    Create username functionality and displaymovement alongsidee with other functionality

---
+++ script.js
@@ -0,0 +1,209 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Sanni Fawaz',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Adu Eniola',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Lateef Idris',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sanni Azeezat',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const calDisplayBalance = function (movement) {
+  const balance = movement.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `${balance}₦`;
+};
+calDisplayBalance(movements);
+
+const calSummaryDisplay = function (movement) {
+  const sumIncome = movement
+    .filter(mov => mov > 0)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumIn.textContent = `${sumIncome}₦`;
+  const sumOutcome = movement
+    .filter(mov => mov < 0)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumOut.textContent = `${sumOutcome}₦`;
+  const sumInterest = movement
+    .filter(mov => mov > 0)
+    .map(mov => mov * 1.2)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumInterest.textContent = `${sumInterest}₦`;
+};
+calSummaryDisplay(movements);
+
+const displayMovement = function (movement) {
+  containerMovements.innerHTML = '';
+  movement.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}₦</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovement(movements);
+
+//Create usernames for accounts
+const createUsernames = function (account) {
+  account.forEach(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
+// Login Functionality
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const calAverageHumanAge = function (ages) {
+//   const humanAge = ages.map(function (age) {
+//     if (age <= 2) {
+//       return 2 * age;
+//     } else {
+//       return 16 + age * 4;
+//     }
+//   });
+//   console.log(humanAge);
+// };
+
+// calAverageHumanAge([10, 2, 1, 5]);
+
+// const breeds = [
+//   {
+//     breed: 'German Shepherd',
+//     averageWeight: 32,
+//     activities: ['fetch', 'swimming'],
+//   },
+//   {
+//     breed: 'Dalmatian',
+//     averageWeight: 24,
+//     activities: ['running', 'fetch', 'agility'],
+//   },
+//   {
+//     breed: 'Labrador',
+//     averageWeight: 28,
+//     activities: ['swimming', 'fetch'],
+//   },
+//   {
+//     breed: 'Beagle',
+//     averageWeight: 12,
+//     activities: ['digging', 'fetch'],
+//   },
+//   {
+//     breed: 'Husky',
+//     averageWeight: 26,
+//     activities: ['running', 'agility', 'swimming'],
+//   },
+//   {
+//     breed: 'Bulldog',
+//     averageWeight: 36,
+//     activities: ['sleeping'],
+//   },
+//   {
+//     breed: 'Poodle',
+//     averageWeight: 18,
+//     activities: ['agility', 'fetch'],
+//   },
+// ];
+
+// const huskyWeight = breeds.find(dog => dog.breed === 'Husky').averageWeight;
+
+// console.log(huskyWeight);
+
+// const dogBothActivities = breeds.find(
+//   dog => dog.activities.includes('running') && dog.activities.includes('fetch')
+// );
+// console.log(dogBothActivities);
+// const allActivities = breeds.flatMap(breed => breed.activities);
+// console.log(allActivities);
+// const uniqueActivities = [...new Set(allActivities)];
+// console.log(uniqueActivities);
+// console.log(uniqueActivities.filter(act => act !== 'swimming'));
+
+// console.log(breeds.every(dog => dog.averageWeight > 10));
+
+// breeds.some(breed => breed.activities.length >= 3);
+
+// const array = 'hello';
+// console.log(
+//   [...array].sort((a, b) => {
+//     if (a > b) return 1;
+//     else return -1;
+//   })
+// );
+
+/////////////////////////////////////////////////
